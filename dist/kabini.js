33e48732bc9392c74002c02ed4e7ef76f0446eee
c7x43t <c7x43t@gmail.com>
2018-10-16 18:08:10 +0300

    bumps package version

--- dist/kabini.js
+++ dist/kabini.js
@@ -1,25 +1,6 @@
 (function () {
 	'use strict';
 
-	class SamplePlugin{
-		constructor(){
-			const a=2;
-			this.a=a;
-		}
-	};
-
-	class C{
-		
-	};
-
-	class B{
-		
-	};
-
-	class A{
-		
-	};
-
 	class Kabini extends HTMLElement{
 		constructor(){
 			
--- src/main.js
+++ src/main.js
@@ -1,7 +1,3 @@
-import './plugins/sample-plugin.js';
-import './plugins/a.js';
-import './plugins/b.js';
-import './plugins/c.js';
 import './modules/kabini.js';
 import './components/to-do.js';
   
