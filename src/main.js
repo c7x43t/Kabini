e6c551babf66a34a49fab61c354cfa9d1ee308e6
c7x43t <c7x43t@gmail.com>
2018-10-16 17:59:00 +0300

    bumps package version

--- src/main.js
+++ src/main.js
@@ -2,5 +2,23 @@ import './plugins/sample-plugin.js';
 import './plugins/a.js';
 import './plugins/b.js';
 import './plugins/c.js';
+class Kabini extends HTMLElement{
+	constructor(){
+		
+	}
+	connectedCallback(){
+		
+	}
+	disconnectedCallback(){
+		
+	}
+	adaptedCallback(){
+		
+	}
+	attributeChangedCallback()
+		
+	}
+}	
+
 
 window.SamplePlugin=SamplePlugin;
