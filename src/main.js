7151f62a73c5f40f9bf28160c6dae9c866c99cf5
c7x43t <c7x43t@gmail.com>
2018-10-16 18:01:18 +0300

    bumps package version

--- src/main.js
+++ src/main.js
@@ -2,23 +2,7 @@ import './plugins/sample-plugin.js';
 import './plugins/a.js';
 import './plugins/b.js';
 import './plugins/c.js';
-class Kabini extends HTMLElement{
-	constructor(){
-		
-	}
-	connectedCallback(){
-		
-	}
-	disconnectedCallback(){
-		
-	}
-	adaptedCallback(){
-		
-	}
-	attributeChangedCallback()
-		
-	}
-}	
+import './modules/kabini.js';
 
 
 window.Kabini=Kabini;
