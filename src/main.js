d948586f9670a9fcff6a80db1973e12938e1c8b5
c7x43t <c7x43t@gmail.com>
2018-10-16 14:22:16 +0300

    bumps package version

--- src/main.js
+++ src/main.js
@@ -2,6 +2,5 @@ import './plugins/sample-plugin.js';
 import './plugins/a.js';
 import './plugins/b.js';
 import './plugins/c.js';
-import camelCase from 'lodash';
 
 window.SamplePlugin=SamplePlugin;
