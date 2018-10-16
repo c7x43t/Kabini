0efa6418cd987260ac5849a2e6feee6a4da797fa
c7x43t <c7x43t@gmail.com>
2018-10-16 18:53:18 +0300

    bumps package version

--- dist/kabini.js
+++ dist/kabini.js
@@ -27,6 +27,7 @@
 	function tagToCamelCase(str){
 		return str.toLowerCase()
 		.split("-")
+		// turn the first letter to uppercase starting at the second segment
 		.map((e,i)=>i>0?e[0].toUpperCase()+e.slice(1,e.length):e)
 		.join("");
 	};
